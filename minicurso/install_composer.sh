df58ec2b38fe4df6ea4ce704a627538ecb42fcd3
Daniel M Brasil <danielmbrasil@protonmail.com>
2022-03-13 16:42:33 -0300

    add composer installer script

---
+++ minicurso/install_composer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# https://getcomposer.org/download/
+#
+# The following script installs the latest version of composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '906a84df04cea2aa72f40b5f787e49f22d4c2f19492ac310e8cba5b96ac8b64115ac402c8cd292b8a03482574915d1a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
+# Move installed program to bin directory
+sudo mv composer.phar /usr/local/bin/composer
