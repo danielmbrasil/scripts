b6af0ffaaae2df87cd7d1b83cfee4fc14db25926
Daniel M Brasil <danielmbrasil@protonmail.com>
2022-03-16 18:03:32 -0300

    add missing command

--- minicurso/install_php_7.sh
+++ minicurso/install_php_7.sh
@@ -3,4 +3,4 @@
 # Install PHP 7.4 and some extensions
 apt update
 apt install zip -y
-php7.4-fpm php7.4-mysql php7.4-mbstring php7.4-xml php7.4-bcmath php7.4-zip -y
+apt install php7.4-fpm php7.4-mysql php7.4-mbstring php7.4-xml php7.4-bcmath php7.4-zip -y
